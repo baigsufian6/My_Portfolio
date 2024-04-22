153824caf32f172aab69d79929c49fe4ba628ac2
baigsufian6 <baigsufian6@gmail.com>
2024-04-22 16:21:47 +0530

    my changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,35 +12,38 @@ export default function Projects() {
           <div className="portfolio-content">
             <div className="portfolio-box">
               <div className='portImg'>
-                <a href="https://github.com/baigsufian6/realTime_codeEditor">
                 <img src={logofullstack} alt="img no" />
-                </a>
               </div>
               <div className="portfoliolayer">
                 <h5>WEB DEVELOPMENT</h5>
                 <p>Real Time Code Editor</p>
+                <a href='https://github.com/baigsufian6/realTime_codeEditor'>
+                <button className='githubBtn'><a href='https://github.com/baigsufian6/realTime_codeEditor'>Github</a></button>
+                </a>
               </div>
             </div>
             <div className="portfolio-box">
               <div className='portImg'>
-                <a href='https://github.com/baigsufian6/Machine-Learning-Projects'>
               <img src={images} alt="img no" />
-              </a>
               </div>
               <div className="portfoliolayer">
                 <h5>MACHINE LEARNING</h5>
                 <p>Online Cab Trip Analysis</p>
+                <a href='https://github.com/baigsufian6/Machine-Learning-Projects'>
+                <button className='githubBtn'><a href='https://github.com/baigsufian6/Machine-Learning-Projects'>Github</a></button>
+                </a>
               </div>
             </div>
             <div className="portfolio-box">
             <div className='portImg'>
-                <a href='https://github.com/baigsufian6/My_Portfolio'>
               <img src={portImage} alt="img no" />
-              </a>
               </div>
               <div className="portfoliolayer">
                 <h5>WEB DEVELOPMENT</h5>
                 <p>My Portfolio</p>
+                <a className='btnGitMain' href='https://github.com/baigsufian6/My_Portfolio'>
+                <button className='githubBtn'><a className='btnGitSmall' href='https://github.com/baigsufian6/My_Portfolio'>Github</a></button>
+                </a>
               </div>
             </div>
           </div>
